90fea64b8557721ca453c3fcf3fbb62b4417f80c
SukruthMS <32141094+SukruthMS@users.noreply.github.com>
2023-03-15 19:09:50 -0400

    import class fix

--- test/model/IMEModelImplTest.java
+++ test/model/IMEModelImplTest.java
@@ -1,27 +1,24 @@
 package model;
 
-import org.junit.Before;
-import org.junit.Test;
-
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 import java.awt.geom.IllegalPathStateException;
 import java.io.FileInputStream;
 import java.util.List;
-
-import controller.imageFileLoader.LoadImage;
-import controller.imageFileLoader.LoadPPM;
-
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import service.imageFileLoader.LoadImage;
+import service.imageFileLoader.LoadPPM;
 
 public class IMEModelImplTest {
+
   IMEModel image;
 
   public IMEModelImplTest() {
     try {
       LoadImage imageLoader = new LoadPPM(new FileInputStream("test.ppm"));
       image = imageLoader.load("test.ppm", "test");
-    } catch(Exception e) {
+    } catch (Exception e) {
       throw new IllegalPathStateException(e.getMessage());
     }
   }
@@ -45,8 +42,8 @@ public class IMEModelImplTest {
   @Test
   public void redGreyScaleImage() {
     IMEModel redScaleImage = image.greyScaleImage(Pixel::getRedComponent);
-    for(int i=0; i<redScaleImage.getImageHeight(); i++) {
-      for(int j=0; j < redScaleImage.getImageWidth(); j++) {
+    for (int i = 0; i < redScaleImage.getImageHeight(); i++) {
+      for (int j = 0; j < redScaleImage.getImageWidth(); j++) {
         int expected = image.getImageData()[i][j].getRedComponent();
         assertEquals(expected, redScaleImage.getImageData()[i][j].getRedComponent());
         assertEquals(expected, redScaleImage.getImageData()[i][j].getBlueComponent());
@@ -58,8 +55,8 @@ public class IMEModelImplTest {
   @Test
   public void blueGreyScaleImage() {
     IMEModel blueScaleImage = image.greyScaleImage(Pixel::getBlueComponent);
-    for(int i=0; i<blueScaleImage.getImageHeight(); i++) {
-      for(int j=0; j < blueScaleImage.getImageWidth(); j++) {
+    for (int i = 0; i < blueScaleImage.getImageHeight(); i++) {
+      for (int j = 0; j < blueScaleImage.getImageWidth(); j++) {
         int expected = image.getImageData()[i][j].getBlueComponent();
         assertEquals(expected, blueScaleImage.getImageData()[i][j].getRedComponent());
         assertEquals(expected, blueScaleImage.getImageData()[i][j].getBlueComponent());
@@ -71,8 +68,8 @@ public class IMEModelImplTest {
   @Test
   public void greenGreyScaleImage() {
     IMEModel greenScaleImage = image.greyScaleImage(Pixel::getGreenComponent);
-    for(int i=0; i<greenScaleImage.getImageHeight(); i++) {
-      for(int j=0; j < greenScaleImage.getImageWidth(); j++) {
+    for (int i = 0; i < greenScaleImage.getImageHeight(); i++) {
+      for (int j = 0; j < greenScaleImage.getImageWidth(); j++) {
         int expected = image.getImageData()[i][j].getGreenComponent();
         assertEquals(expected, greenScaleImage.getImageData()[i][j].getRedComponent());
         assertEquals(expected, greenScaleImage.getImageData()[i][j].getBlueComponent());
@@ -84,8 +81,8 @@ public class IMEModelImplTest {
   @Test
   public void valueGreyScaleImage() {
     IMEModel valueScaleImage = image.greyScaleImage(Pixel::getValue);
-    for(int i=0; i<valueScaleImage.getImageHeight(); i++) {
-      for(int j=0; j < valueScaleImage.getImageWidth(); j++) {
+    for (int i = 0; i < valueScaleImage.getImageHeight(); i++) {
+      for (int j = 0; j < valueScaleImage.getImageWidth(); j++) {
         int expected = image.getImageData()[i][j].getValue();
         assertEquals(expected, valueScaleImage.getImageData()[i][j].getRedComponent());
         assertEquals(expected, valueScaleImage.getImageData()[i][j].getBlueComponent());
@@ -97,8 +94,8 @@ public class IMEModelImplTest {
   @Test
   public void intensityGreyScaleImage() {
     IMEModel intensityScaleImage = image.greyScaleImage(Pixel::getIntensity);
-    for(int i=0; i<intensityScaleImage.getImageHeight(); i++) {
-      for(int j=0; j < intensityScaleImage.getImageWidth(); j++) {
+    for (int i = 0; i < intensityScaleImage.getImageHeight(); i++) {
+      for (int j = 0; j < intensityScaleImage.getImageWidth(); j++) {
         int expected = image.getImageData()[i][j].getIntensity();
         assertEquals(expected, intensityScaleImage.getImageData()[i][j].getRedComponent());
         assertEquals(expected, intensityScaleImage.getImageData()[i][j].getBlueComponent());
@@ -110,8 +107,8 @@ public class IMEModelImplTest {
   @Test
   public void lumaGreyScaleImage() {
     IMEModel lumaScaleImage = image.greyScaleImage(Pixel::getLuma);
-    for(int i=0; i<lumaScaleImage.getImageHeight(); i++) {
-      for(int j=0; j < lumaScaleImage.getImageWidth(); j++) {
+    for (int i = 0; i < lumaScaleImage.getImageHeight(); i++) {
+      for (int j = 0; j < lumaScaleImage.getImageWidth(); j++) {
         int expected = image.getImageData()[i][j].getLuma();
         assertEquals(expected, lumaScaleImage.getImageData()[i][j].getRedComponent());
         assertEquals(expected, lumaScaleImage.getImageData()[i][j].getBlueComponent());
@@ -123,7 +120,7 @@ public class IMEModelImplTest {
   @Test
   public void horizontalFlipImage() {
     IMEModel horizontal = image.horizontalFlipImage();
-    for (int i=0; i< horizontal.getImageHeight(); i++) {
+    for (int i = 0; i < horizontal.getImageHeight(); i++) {
       assertArrayEquals(reverse(image.getImageData()[i]), horizontal.getImageData()[i]);
     }
   }
@@ -131,23 +128,24 @@ public class IMEModelImplTest {
   @Test
   public void verticalFlipImage() {
     IMEModel vertical = image.verticalFlipImage();
-    for (int i=0; i< vertical.getImageHeight()/2; i++) {
-      assertArrayEquals(image.getImageData()[image.getImageHeight() -1 - i], vertical.getImageData()[i]);
+    for (int i = 0; i < vertical.getImageHeight() / 2; i++) {
+      assertArrayEquals(image.getImageData()[image.getImageHeight() - 1 - i],
+          vertical.getImageData()[i]);
     }
   }
 
   @Test
   public void alterBrightness() {
     int delta = 100;
-    IMEModel brighten = image.alterBrightness( delta );
-    for(int i=0; i<brighten.getImageHeight(); i++) {
-      for(int j=0; j < brighten.getImageWidth(); j++) {
-        assertEquals(Math.min( image.getImageData()[i][j].getRedComponent() + delta , 255),
-                brighten.getImageData()[i][j].getRedComponent());
-        assertEquals(Math.min( image.getImageData()[i][j].getBlueComponent() + delta , 255),
-                brighten.getImageData()[i][j].getBlueComponent());
-        assertEquals(Math.min( image.getImageData()[i][j].getGreenComponent() + delta , 255),
-                brighten.getImageData()[i][j].getGreenComponent());
+    IMEModel brighten = image.alterBrightness(delta);
+    for (int i = 0; i < brighten.getImageHeight(); i++) {
+      for (int j = 0; j < brighten.getImageWidth(); j++) {
+        assertEquals(Math.min(image.getImageData()[i][j].getRedComponent() + delta, 255),
+            brighten.getImageData()[i][j].getRedComponent());
+        assertEquals(Math.min(image.getImageData()[i][j].getBlueComponent() + delta, 255),
+            brighten.getImageData()[i][j].getBlueComponent());
+        assertEquals(Math.min(image.getImageData()[i][j].getGreenComponent() + delta, 255),
+            brighten.getImageData()[i][j].getGreenComponent());
       }
     }
   }
@@ -155,15 +153,15 @@ public class IMEModelImplTest {
   @Test
   public void darkenImage() {
     int delta = -100;
-    IMEModel brighten = image.alterBrightness( delta );
-    for(int i=0; i<brighten.getImageHeight(); i++) {
-      for(int j=0; j < brighten.getImageWidth(); j++) {
-        assertEquals(Math.max( image.getImageData()[i][j].getRedComponent() + delta , 0),
-                brighten.getImageData()[i][j].getRedComponent());
-        assertEquals(Math.max( image.getImageData()[i][j].getBlueComponent() + delta , 0),
-                brighten.getImageData()[i][j].getBlueComponent());
-        assertEquals(Math.max( image.getImageData()[i][j].getGreenComponent() + delta , 0),
-                brighten.getImageData()[i][j].getGreenComponent());
+    IMEModel brighten = image.alterBrightness(delta);
+    for (int i = 0; i < brighten.getImageHeight(); i++) {
+      for (int j = 0; j < brighten.getImageWidth(); j++) {
+        assertEquals(Math.max(image.getImageData()[i][j].getRedComponent() + delta, 0),
+            brighten.getImageData()[i][j].getRedComponent());
+        assertEquals(Math.max(image.getImageData()[i][j].getBlueComponent() + delta, 0),
+            brighten.getImageData()[i][j].getBlueComponent());
+        assertEquals(Math.max(image.getImageData()[i][j].getGreenComponent() + delta, 0),
+            brighten.getImageData()[i][j].getGreenComponent());
       }
     }
   }
@@ -171,26 +169,26 @@ public class IMEModelImplTest {
   @Test
   public void rgbSplit() {
     List<IMEModel> images = image.rgbSplit();
-    for(int i=0; i<image.getImageHeight(); i++) {
-      for(int j=0; j < image.getImageWidth(); j++) {
+    for (int i = 0; i < image.getImageHeight(); i++) {
+      for (int j = 0; j < image.getImageWidth(); j++) {
         assertEquals(image.getImageData()[i][j].getRedComponent(),
-                images.get(0).getImageData()[i][j].getRedComponent());
+            images.get(0).getImageData()[i][j].getRedComponent());
         assertEquals(image.getImageData()[i][j].getRedComponent(),
-                images.get(0).getImageData()[i][j].getBlueComponent());
+            images.get(0).getImageData()[i][j].getBlueComponent());
         assertEquals(image.getImageData()[i][j].getRedComponent(),
-                images.get(0).getImageData()[i][j].getGreenComponent());
+            images.get(0).getImageData()[i][j].getGreenComponent());
         assertEquals(image.getImageData()[i][j].getGreenComponent(),
-                images.get(1).getImageData()[i][j].getRedComponent());
+            images.get(1).getImageData()[i][j].getRedComponent());
         assertEquals(image.getImageData()[i][j].getGreenComponent(),
-                images.get(1).getImageData()[i][j].getBlueComponent());
+            images.get(1).getImageData()[i][j].getBlueComponent());
         assertEquals(image.getImageData()[i][j].getGreenComponent(),
-                images.get(1).getImageData()[i][j].getGreenComponent());
+            images.get(1).getImageData()[i][j].getGreenComponent());
         assertEquals(image.getImageData()[i][j].getBlueComponent(),
-                images.get(2).getImageData()[i][j].getRedComponent());
+            images.get(2).getImageData()[i][j].getRedComponent());
         assertEquals(image.getImageData()[i][j].getBlueComponent(),
-                images.get(2).getImageData()[i][j].getBlueComponent());
+            images.get(2).getImageData()[i][j].getBlueComponent());
         assertEquals(image.getImageData()[i][j].getBlueComponent(),
-                images.get(2).getImageData()[i][j].getGreenComponent());
+            images.get(2).getImageData()[i][j].getGreenComponent());
       }
     }
   }
@@ -199,14 +197,14 @@ public class IMEModelImplTest {
   public void combineRGBImage() {
     List<IMEModel> images = image.rgbSplit();
     IMEModel combinedRGBImage = images.get(0).combineRGBImage(images.get(1), images.get(2));
-    for(int i=0; i<combinedRGBImage.getImageHeight(); i++) {
-      for(int j=0; j < combinedRGBImage.getImageWidth(); j++) {
+    for (int i = 0; i < combinedRGBImage.getImageHeight(); i++) {
+      for (int j = 0; j < combinedRGBImage.getImageWidth(); j++) {
         assertEquals(combinedRGBImage.getImageData()[i][j].getRedComponent(),
-                images.get(0).getImageData()[i][j].getRedComponent());
+            images.get(0).getImageData()[i][j].getRedComponent());
         assertEquals(combinedRGBImage.getImageData()[i][j].getGreenComponent(),
-                images.get(1).getImageData()[i][j].getGreenComponent());
+            images.get(1).getImageData()[i][j].getGreenComponent());
         assertEquals(combinedRGBImage.getImageData()[i][j].getBlueComponent(),
-                images.get(2).getImageData()[i][j].getBlueComponent());
+            images.get(2).getImageData()[i][j].getBlueComponent());
       }
     }
   }
