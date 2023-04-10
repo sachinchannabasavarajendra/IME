1c22c949791342f6ed18babef0a37a27b1b5f8c3
SukruthMS <32141094+SukruthMS@users.noreply.github.com>
2023-04-09 21:19:40 -0400

    Histogram for images

--- src/controller/GRIMEController.java
+++ src/controller/GRIMEController.java
@@ -1,12 +1,13 @@
 package controller;
 
+import java.io.FileNotFoundException;
 import view.IView;
 
 public class GRIMEController implements Features {
 
   private IView view;
 
-  public void setView(IView v) {
+  public void setView(IView v) throws FileNotFoundException {
     view = v;
     view.addFeatures(this);
   }
--- src/view/IView.java
+++ src/view/IView.java
@@ -1,7 +1,10 @@
 package view;
 
 import controller.Features;
+import model.IMEModel;
 
 public interface IView {
   void addFeatures(Features features);
+
+  void drawHistogram(IMEModel image);
 }
