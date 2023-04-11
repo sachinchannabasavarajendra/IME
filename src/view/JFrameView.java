89283c468e9a82cdaf7a7d735a7047ff4596b718
SukruthMS <32141094+SukruthMS@users.noreply.github.com>
2023-04-11 18:52:26 -0400

    intensity component fix

--- src/view/JFrameView.java
+++ src/view/JFrameView.java
@@ -230,8 +230,8 @@ public class JFrameView extends JFrame implements IView {
         redHistogram[color.getRed()]++;
         greenHistogram[color.getGreen()]++;
         blueHistogram[color.getBlue()]++;
-        intensityHistogram[(int) (0.299 * color.getRed() + 0.587 * color.getGreen()
-            + 0.114 * color.getBlue())]++;
+        intensityHistogram[(int) Math.round(
+            ((double) color.getRed() + color.getGreen() + color.getBlue()) / 3)]++;
       }
     }
 
